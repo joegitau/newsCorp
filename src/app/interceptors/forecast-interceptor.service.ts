7db1e16445d410f25279e43a7d5d268514c7c19c
joseph.gitau <joseph.gitau@canter.fi>
2020-04-25 21:50:38 +0300

    add http interceptor to set HttpParams

--- src/app/interceptors/forecast-interceptor.service.ts
+++ src/app/interceptors/forecast-interceptor.service.ts
@@ -1,33 +1,33 @@
-// import { Injectable } from '@angular/core';
-// import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpParams } from '@angular/common/http';
-// import { Observable } from 'rxjs';
-// import { map } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map, mergeMap } from 'rxjs/operators';
 
-// import { ForecastService } from '../weather/forecast.service';
+import { ForecastService } from '../weather/forecast.service';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class ForecastInterceptorService implements HttpInterceptor {
+@Injectable({
+  providedIn: 'root'
+})
+export class ForecastInterceptorService implements HttpInterceptor {
 
-//   constructor(private forecastService: ForecastService) { }
+  constructor(private forecastService: ForecastService) { }
 
-//   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-//     // let params: HttpParams;
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const params = this.forecastService
+      .getCurrentLocation()
+      .pipe(
+        map(coords => {
+          return new HttpParams()
+            .set('lat', String(coords.latitude))
+            .set('lon', String(coords.longitude))
+            .set('units', 'metric')
+            .set('appid', 'ffda23cc6f49430837eac691ed4e8754')
+        })
+      )
 
-//     const params = this.forecastService
-//       .getCurrentLocation()
-//       .pipe(
-//         map(coords => {
-//           return new HttpParams()
-//             .set('lat', String(coords.latitude))
-//             .set('lon', String(coords.longitude))
-//             .set('units', 'metrics')
-//             .set('appid', 'ffda23cc6f49430837eac691ed4e8754')
-//         })
-//       )
-
-//     const clonedReq = req.clone({ params: params });
-//     return next.handle(clonedReq);
-//   }
-// }
+    return params.pipe(mergeMap(ps => {
+      const clonedReq = req.clone({ params: ps });
+      return next.handle(clonedReq);
+    }));
+  }
+}
